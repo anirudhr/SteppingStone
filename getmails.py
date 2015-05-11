f2baebf30cb563340b8a9c5fcfa027818ed5e829
Anirudh Ramachandran <anirudhr1989@gmail.com>
2015-05-11 17:31:00 -0400

    Taking the IMAP approach

--- getmails.py
+++ getmails.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+
+import configparser
+config = configparser.ConfigParser()
+config.read('mailcreds.cfg')
+USER = config.get('GMail', 'emailid')
+PASS = config.get('GMail', 'password')
+
+import imaplib
+server = imaplib.IMAP4_SSL('imap.gmail.com')
+server.login(USER, PASS)
+
+from pprint import pprint
+pprint(server.list())
